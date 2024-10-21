7f3ae3c5c05112b02e7f6afa70b8d29e9c5751a2
Jonathan Silva <jonathan.gmsilva@gmail.com>
2024-10-21 08:58:29 -0300

    add migrations

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProfileApi.Database;
+using ProfileApi.Extensions;
+using ProfileApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +13,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme);
+
+
+builder.Services.AddIdentityCore<User>()
+  .AddEntityFrameworkStores<ApplicationDbContext>()
+  .AddApiEndpoints();
+
+
+// Add DbContext sqlite
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+  options.UseSqlite("Data Source=app.db");
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,12 +35,14 @@ if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
   app.UseSwaggerUI();
+
+  app.ApplyMigrations();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapIdentityApi<User>();
 
 app.Run();
