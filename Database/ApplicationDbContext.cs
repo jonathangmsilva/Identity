32c0e48af64419a940b99b6451c662371f3b749c
Jonathan Silva <jonathan.gmsilva@gmail.com>
2024-10-21 08:57:54 -0300

    rename appdbcontext

---
+++ Database/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProfileApi.Models;
+
+namespace ProfileApi.Database
+{
+  public class ApplicationDbContext : IdentityDbContext<User>
+  {
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      base.OnModelCreating(builder);
+
+      builder.Entity<User>(entity =>
+      {
+        entity.Property(e => e.FirstName).HasMaxLength(100);
+        entity.Property(e => e.LastName).HasMaxLength(100);
+      });
+    }
+  }
+}
