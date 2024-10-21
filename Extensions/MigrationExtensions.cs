4a8b274d41ac3b767455abe37bc7fd7ebdda90c0
Jonathan Silva <jonathan.gmsilva@gmail.com>
2024-10-21 08:57:40 -0300

    add migration extensions

---
+++ Extensions/MigrationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProfileApi.Database;
+
+namespace ProfileApi.Extensions
+{
+  public static class MigrationExtensions
+  {
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+      using var scope = app.ApplicationServices.CreateScope();
+
+      var services = scope.ServiceProvider;
+      var dbContext = services.GetRequiredService<ApplicationDbContext>();
+
+      dbContext.Database.Migrate();
+    }
+  }
+}
